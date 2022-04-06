a492efdd1a18c87a4325aee139a4980649970c64
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-04-06 14:09:55 +0200

    ultra conscis

--- get_next_line.c
+++ get_next_line.c
@@ -5,154 +5,55 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: supersko <ndionis@student.42mulhouse.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/15 19:18:28 by supersko          #+#    #+#             */
-/*   Updated: 2022/03/22 14:35:50 by supersko         ###   ########.fr       */
+/*   Created: 2022/04/05 15:17:56 by supersko          #+#    #+#             */
+/*   Updated: 2022/04/06 14:05:33 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-unsigned int ft_strlen(char *s)
-{
-    unsigned int    ii;
-
-    ii = 0;
-    while (s[ii])
-        ii++;
-    return (ii);
-}
-
-void ft_strcpy(char *dest, char *src)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		++i;
-	}
-	dest[i] = '\0';
-}
-
-void  ft_strcat(char *dst, char *src)
-{
-    size_t  dstr_len;
-
-    dstr_len = ft_strlen(dst);
-    ft_strcpy(dst + dstr_len, (char *) src);
-}
-
-void	ft_bzero(void *b, size_t n)
-{
-	unsigned char	*dest;
-	size_t			i;
-
-	dest = b;
-	i = 0;
-	while (i++ < n)
-		*dest++ = 0;
-}
-
-void		*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*mem;
-
-	if (!(mem = malloc(nmemb * size)))
-		return (NULL);
-	ft_bzero(mem, (nmemb * size));
-	return (mem);
-}
-
-char	*ft_lst_to_str(t_list *item)
-{
-	size_t	len;
-	char	*ret;
-	t_list	**to_delete;
-
-	to_delete = &item;
-	len = ft_lstsize(item);
-	ret = calloc(len, BUFFER_SIZE);
-	if (!ret)
-		return (NULL);
-	while (item != NULL)
-	{
-		ft_strcat(ret, item->line);
-		item = item->next;
-	}
-	ft_lstclear(to_delete, free);
-	return (ret);
-}
-
-
 char	*get_next_line(int fd)
 {
-	t_list		*str_link;
-	t_list		*new_str_link;
-	char		c;
-	size_t		ii;
+	char	c;
+	char	*line;
+	int		len;
 
-	if (fd == -1)
+	if (!fd || fd < 0 || fd > MAX_FD)
 		return (NULL);
-	str_link = ft_lstnew((char *) ft_calloc(1, sizeof(BUFFER_SIZE)));
-	if (!str_link)
-		return (NULL);
-	if (BUFFER_SIZE != 0)
+	len = 0;
+	line = NULL;
+	while (1)
 	{
-		ii = 0;
-		new_str_link = str_link;
-		while (0 < read(fd, &c, 1))
-		{
-			if (c == '\n')
-				return (ft_lst_to_str(str_link));
-			else if (c == 0)
-				return (ft_lst_to_str(str_link));
-			else if (ii < BUFFER_SIZE - 2)
-				new_str_link->line[ii++] = c;
-			else
-			{
-				new_str_link->line[ii] = c;
-				new_str_link = ft_lstnew(malloc(sizeof(BUFFER_SIZE)));
-				if (new_str_link == NULL)
-					return (NULL);
-				ft_lstadd_back(&str_link, new_str_link);
-				ii = 0;
-			}
-		}
+		if (read(fd, &c, 1) > 0)
+			line = ft_realloc_cat(line, c, &len);
+		else
+			(c = '\0');
+		if (c == '\n' || !c)
+			return (line);
 	}
+	if (line)
+		free(line);
 	return (NULL);
 }
 
+/*
+#include <stdio.h>
 int main(int argc, char *argv[])
 {
-/*
-	t_list	*n0;
-
-	n0 = ft_lstnew("voici une chaine");
-	ft_lstadd_back(&n0, ft_lstnew("une seconde"));
-	ft_lstadd_back(&n0, ft_lstnew("\n"));
-	printf("%s\n", ft_lst_to_str(n0));
-*/
-	int fd;
+	int		fd;
 	char	*line;
-	int ii = 0;
 
-	if (argc == 2)
+	(void)argc;
+	if (!argv[1])
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s", line);
+	while (line)
 	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd != -1)
-		{
-			line = get_next_line(fd);
-			while (line != NULL)
-			{
-				printf("%s\n", line);
-				line = get_next_line(fd);
-			}
-		}
+		line = get_next_line(fd);
+		printf("%s", line);
 	}
-	else
-	{
-		printf("%s\n", "fichier manquant");
-	}
-	return (0);
+	close(fd);
 }
+*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,30 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: supersko <ndionis@student.42mulhouse.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/05 15:19:00 by supersko          #+#    #+#             */
-/*   Updated: 2022/04/06 14:04:10 by supersko         ###   ########.fr       */
+/*   Created: 2022/04/06 14:02:10 by supersko          #+#    #+#             */
+/*   Updated: 2022/04/06 14:02:38 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
-# endif
-# undef BUFFER_SIZE
-# define BUFFER_SIZE 1
-# ifndef MAX_FD
-#  define MAX_FD 256
-# endif
+#include "get_next_line.h"
 
-char	*get_next_line(int fd);
-char	*ft_realloc_cat(char *line, char c, int *len);
+char	*ft_realloc_cat(char *line, char c, int *len)
+{
+	char	*ret;
+	int		i;
 
-#endif
+	i = 0;
+	ret = (char *) malloc(*len + 2);
+	if (!ret)
+		return (NULL);
+	while (i < *len && line)
+	{
+		ret[i] = line[i];
+		i++;
+	}
+	ret[*len] = c;
+	ret[++(*len)] = '\0';
+	if (line)
+		free(line);
+	return (ret);
+}
